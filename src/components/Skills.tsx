8694e74adcb0f399655e77204a530be5011d8a48
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-04-17 23:59:29 +0545

    feat: enhance Skills component with frameworks, database, and hosting sections

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -28,10 +28,85 @@ const languages: { name: string; icon: ReactNode }[] = [
     icon: <Icon icon="skill-icons:cpp" width="36" height="36" />,
   },
 ];
+
+const frameworks: { name: string; icon: ReactNode }[] = [
+  {
+    name: "React",
+    icon: <Icon icon="skill-icons:react-dark" width="36" height="36" />,
+  },
+  {
+    name: "NextJs",
+    icon: <Icon icon="skill-icons:nextjs-light" width="36" height="36" />,
+  },
+  {
+    name: "ExpressJs",
+    icon: <Icon icon="skill-icons:expressjs-dark" width="36" height="36" />,
+  },
+  {
+    name: "NodeJs",
+    icon: <Icon icon="skill-icons:nodejs-dark" width="36" height="36" />,
+  },
+  {
+    name: "Tailwind",
+    icon: <Icon icon="skill-icons:tailwindcss-dark" width="36" height="36" />,
+  },
+  {
+    name: "Material UI",
+    icon: <Icon icon="skill-icons:materialui-dark" width="36" height="36" />,
+  },
+  {
+    name: "Vite",
+    icon: <Icon icon="skill-icons:vite-dark" width="36" height="36" />,
+  },
+  {
+    name: "Bootstrap",
+    icon: <Icon icon="skill-icons:bootstrap" width="36" height="36" />,
+  },
+  {
+    name: "Shadcn",
+    icon: <Icon icon="skill-icons:shadcn-dark" width="36" height="36" />,
+  },
+];
+const database: { name: string; icon: ReactNode }[] = [
+  {
+    name: "Mysql",
+    icon: <Icon icon="skill-icons:mysql-light" width="36" height="36" />,
+  },
+  {
+    name: "MongoDB",
+    icon: <Icon icon="skill-icons:mongodb" width="36" height="36" />,
+  },
+
+  {
+    name: "Prisma",
+    icon: <Icon icon="skill-icons:prisma" width="36" height="36" />,
+  },
+];
+const hosting: { name: string; icon: ReactNode }[] = [
+  {
+    name: "Vercel",
+    icon: <Icon icon="skill-icons:vercel-dark" width="36" height="36" />,
+  },
+
+  {
+    name: "Render",
+    icon: <Icon icon="skill-icons:render" width="36" height="36" />,
+  },
+  {
+    name: "Cloudflare",
+    icon: <Icon icon="skill-icons:cloudflare" width="36" height="36" />,
+  },
+  {
+    name: "Netlify",
+    icon: (
+      <Icon icon="material-icon-theme:netlify-light" width="36" height="36" />
+    ),
+  },
+];
 const Skills = () => {
   return (
     <div className="ml-24">
-      <div className="flex flex-col ">
+      <div className="flex flex-col gap-10">
         <div>Skills</div>
         <div className="flex flex-col gap-5">
           <div className="font-bold text-lg">Languages</div>
@@ -47,9 +122,51 @@ const Skills = () => {
             })}
           </div>
         </div>
-        <div>Frameworks and Libraries</div>
-        <div>Database amd ORM</div>
-        <div>Hosting and SAAS</div>
+        <div>
+          <div className="flex flex-col gap-5">
+            <div className="font-bold text-lg">Frameworks and Libraries</div>
+            <div className="flex  gap-5">
+              {frameworks.map((item) => {
+                return (
+                  <SkillsCard
+                    name={item.name}
+                    icon={item.icon}
+                    key={item.name}
+                  ></SkillsCard>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>Database amd ORM</div>
+          <div className="flex  gap-5">
+            {database.map((item) => {
+              return (
+                <SkillsCard
+                  name={item.name}
+                  icon={item.icon}
+                  key={item.name}
+                ></SkillsCard>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <div>Hosting and SAAS</div>
+          <div className="flex  gap-5">
+            {hosting.map((item) => {
+              return (
+                <SkillsCard
+                  name={item.name}
+                  icon={item.icon}
+                  key={item.name}
+                ></SkillsCard>
+              );
+            })}
+          </div>
+        </div>
+
         <div>Others</div>
       </div>
     </div>
