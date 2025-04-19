426259407144239cbf77ce5b1772b7f3274a6ddc
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-04-19 21:50:59 +0545

    add ProjectsCard component and render project list in Projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,75 @@
+import ProjectsCard from "./ProjectsCard";
+
+const myProjects = [
+  {
+    image: "/park.png",
+    desc: "A full-featured system to manage parking slots efficiently, with entry logs, space tracking, and user dashboard.",
+    link: "https://parkify.samikshyachhetri.com.np/",
+    name: "Parking Management System",
+  },
+  {
+    image: "/lms.png",
+    desc: "A digital solution for managing library books, users, and borrow history with ease.",
+    link: "https://lms.samikshyachhetri.com.np/",
+    name: "Library Management System",
+  },
+  {
+    image: "/recipe.png",
+    desc: "Explore delicious recipes with this clean and responsive app featuring search and step-by-step guides.",
+    link: "https://recipe-app-nine-azure.vercel.app/",
+    name: "Recipe App",
+  },
+  {
+    image: "/expense.png",
+    desc: "Track your daily spending and manage your personal budget with this minimal expense tracker.",
+    link: "https://samikshyachhetri.github.io/Expense-Tracker/",
+    name: "Expense Tracker",
+  },
+  {
+    image: "/hangman.png",
+    desc: "A fun and interactive Hangman game built with vanilla JavaScript for word-guessing challenges.",
+    link: "https://samikshyachhetri.github.io/Hangman-game/",
+    name: "Hangman Game",
+  },
+  {
+    image: "/encryption.png",
+    desc: "Encrypt and decrypt messages with this simple text security tool using basic cryptographic logic.",
+    link: "https://samikshyachhetri.github.io/Encryption-Decryption/",
+    name: "Text-Encryption",
+  },
+  {
+    image: "/ttt.png",
+    desc: "Play the classic Tic Tac Toe game in a clean UI with smooth and responsive gameplay.",
+    link: "https://tictactoe.samikshyachhetri.com.np/",
+    name: "Tic Tac Toe",
+  },
+  {
+    image: "/calc.png",
+    desc: "A responsive calculator app handling all basic arithmetic operations with a smooth interface.",
+    link: "https://samikshyachhetri.github.io/Calculator/",
+    name: "Calculator",
+  },
+];
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="my-10 md:px-28  flex flex-col gap-10 bg-gradient-to-br from-blue-50 via-white to-purple-10">
+      <h2 className="text-center text-3xl font-bold text-gray-800 dark:text-white my-5">
+        My Projects
+      </h2>
+      <div className="flex flex-wrap justify-center gap-8">
+        {myProjects.map((item, index) => (
+          <ProjectsCard
+            key={index}
+            image={item.image}
+            name={item.name || ""}
+            desc={item.desc}
+            link={item.link}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
---
+++ src/components/ProjectsCard.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react";
+import { Button } from "./ui/button";
+
+interface ProjectsCardProps {
+  image: string;
+  desc: string;
+  link: string;
+  name?: string;
+}
+const ProjectsCard: FC<ProjectsCardProps> = ({ image, desc, link, name }) => {
+  return (
+    <div className="w-64  rounded-xl shadow-lg bg-white dark:bg-neutral-900 border border-gray-200 dark:border-neutral-800 transition hover:scale-105 hover:shadow-xl duration-300">
+      <img
+        src={image}
+        alt={name || "Project Image"}
+        className="w-full h-36 object-cover"
+      />
+      <div className="p-4 flex flex-col gap-3 h-56">
+        {name && (
+          <h3 className="text-lg font-bold text-neutral-800 dark:text-white">
+            {name}
+          </h3>
+        )}
+        <div className="flex flex-col justify-between h-full gap-3">
+          <p className="text-sm text-gray-600 dark:text-neutral-300">{desc}</p>
+          <Button className="bg-blue-700 w-full hover:bg-blue-600">
+            <a
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className=" text-sm  text-white  "
+            >
+              Visit Site
+            </a>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsCard;
