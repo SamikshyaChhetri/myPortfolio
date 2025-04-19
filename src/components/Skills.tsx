8fa0ca26f80cd0182cd7a5f97a0500050616dd16
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-04-19 14:20:46 +0545

    refactor Skills component to include IconCloudSection and streamline code structure

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,8 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { ReactNode } from "react";
+import IconCloudSection from "./IconCloud";
 import SkillsCard from "./SkillsCard";
-const languages: { name: string; icon: ReactNode }[] = [
+
+const languages = [
   {
     name: "Typescript",
     icon: <Icon icon="skill-icons:typescript" width="36" height="36" />,
@@ -14,20 +15,12 @@ const languages: { name: string; icon: ReactNode }[] = [
     name: "HTML",
     icon: <Icon icon="skill-icons:html" width="36" height="36" />,
   },
-  {
-    name: "CSS",
-    icon: <Icon icon="skill-icons:css" width="36" height="36" />,
-  },
-  {
-    name: "C",
-    icon: <Icon icon="skill-icons:c" width="36" height="36" />,
-  },
-  {
-    name: "C++",
-    icon: <Icon icon="skill-icons:cpp" width="36" height="36" />,
-  },
+  { name: "CSS", icon: <Icon icon="skill-icons:css" width="36" height="36" /> },
+  { name: "C", icon: <Icon icon="skill-icons:c" width="36" height="36" /> },
+  { name: "C++", icon: <Icon icon="skill-icons:cpp" width="36" height="36" /> },
 ];
-const frameworks: { name: string; icon: ReactNode }[] = [
+
+const frameworks = [
   {
     name: "React",
     icon: <Icon icon="skill-icons:react-dark" width="36" height="36" />,
@@ -65,7 +58,8 @@ const frameworks: { name: string; icon: ReactNode }[] = [
     icon: <Icon icon="simple-icons:shadcnui" width="36" height="36" />,
   },
 ];
-const database: { name: string; icon: ReactNode }[] = [
+
+const database = [
   {
     name: "Mysql",
     icon: <Icon icon="skill-icons:mysql-light" width="36" height="36" />,
@@ -74,18 +68,17 @@ const database: { name: string; icon: ReactNode }[] = [
     name: "MongoDB",
     icon: <Icon icon="skill-icons:mongodb" width="36" height="36" />,
   },
-
   {
     name: "Prisma",
     icon: <Icon icon="skill-icons:prisma" width="36" height="36" />,
   },
 ];
-const hosting: { name: string; icon: ReactNode }[] = [
+
+const hosting = [
   {
     name: "Vercel",
     icon: <Icon icon="skill-icons:vercel-dark" width="36" height="36" />,
   },
-
   {
     name: "Render",
     icon: <Icon icon="simple-icons:render" width="36" height="36" />,
@@ -101,19 +94,14 @@ const hosting: { name: string; icon: ReactNode }[] = [
     ),
   },
 ];
-const others: { name: string; icon: ReactNode }[] = [
-  {
-    name: "Git",
-    icon: <Icon icon="skill-icons:git" width="36" height="36" />,
-  },
+
+const others = [
+  { name: "Git", icon: <Icon icon="skill-icons:git" width="36" height="36" /> },
   {
     name: "Github",
     icon: <Icon icon="logos:github-icon" width="36" height="36" />,
   },
-  {
-    name: "Zod",
-    icon: <Icon icon="logos:zod" width="36" height="36" />,
-  },
+  { name: "Zod", icon: <Icon icon="logos:zod" width="36" height="36" /> },
   {
     name: "Eslint",
     icon: <Icon icon="devicon:eslint" width="36" height="36" />,
@@ -122,103 +110,69 @@ const others: { name: string; icon: ReactNode }[] = [
     name: "Nodemon",
     icon: <Icon icon="devicon:nodemon" width="36" height="36" />,
   },
-  {
-    name: "Figma",
-    icon: <Icon icon="logos:figma" width="36" height="36" />,
-  },
-  {
-    name: "Canva",
-    icon: <Icon icon="devicon:canva" width="36" height="36" />,
-  },
+  { name: "Figma", icon: <Icon icon="logos:figma" width="36" height="36" /> },
+  { name: "Canva", icon: <Icon icon="devicon:canva" width="36" height="36" /> },
 ];
 
 const Skills = () => {
   return (
-    <div className="ml-24">
-      <div className="flex flex-col gap-10">
-        <div className="flex flex-row items-center justify-center font-bold text-[30px] pt-20 text-blue-700">
-          <Icon
-            icon="fluent-color:laptop-24"
-            width="50"
-            height="50"
-            className="w-fit"
-          />
-          Skills
-        </div>
-        <div className="flex flex-col gap-5">
-          <div className="font-bold text-lg">Languages</div>
-          <div className="flex  gap-5">
-            {languages.map((item) => {
-              return (
-                <SkillsCard
-                  name={item.name}
-                  icon={item.icon}
-                  key={item.name}
-                ></SkillsCard>
-              );
-            })}
+    <div className="flex gap-1">
+      <div className="ml-28">
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-row items-center justify-center font-bold text-[30px] pt-20 text-blue-700">
+            <Icon icon="fluent-color:laptop-24" width="50" height="50" />
+            Skills
           </div>
-        </div>
-        <div>
-          <div className="flex flex-col gap-5">
+
+          <div className="flex flex-col gap-3">
+            <div className="font-bold text-lg">Languages</div>
+            <div className="flex gap-5 flex-wrap">
+              {languages.map((item) => (
+                <SkillsCard name={item.name} icon={item.icon} key={item.name} />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3">
             <div className="font-bold text-lg">Frameworks and Libraries</div>
-            <div className="flex  gap-5 flex-wrap w-[50%]">
-              {frameworks.map((item) => {
-                return (
-                  <SkillsCard
-                    name={item.name}
-                    icon={item.icon}
-                    key={item.name}
-                  ></SkillsCard>
-                );
-              })}
+            <div className="flex gap-5 flex-wrap ">
+              {frameworks.map((item) => (
+                <SkillsCard name={item.name} icon={item.icon} key={item.name} />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <div className="font-bold text-lg">Database and ORM</div>
+            <div className="flex gap-5">
+              {database.map((item) => (
+                <SkillsCard name={item.name} icon={item.icon} key={item.name} />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <div className="font-bold text-lg">Hosting and SAAS</div>
+            <div className="flex gap-5">
+              {hosting.map((item) => (
+                <SkillsCard name={item.name} icon={item.icon} key={item.name} />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <div className="font-bold text-lg">Others</div>
+            <div className="flex gap-5 flex-wrap ">
+              {others.map((item) => (
+                <SkillsCard name={item.name} icon={item.icon} key={item.name} />
+              ))}
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-5">
-          <div className="font-bold text-lg">Database amd ORM</div>
-          <div className="flex  gap-5">
-            {database.map((item) => {
-              return (
-                <SkillsCard
-                  name={item.name}
-                  icon={item.icon}
-                  key={item.name}
-                ></SkillsCard>
-              );
-            })}
-          </div>
-        </div>
-        <div className="flex flex-col gap-5">
-          <div className="font-bold text-lg">Hosting and SAAS</div>
-          <div className="flex  gap-5">
-            {hosting.map((item) => {
-              return (
-                <SkillsCard
-                  name={item.name}
-                  icon={item.icon}
-                  key={item.name}
-                ></SkillsCard>
-              );
-            })}
-          </div>
-        </div>
-        <div className="flex flex-col gap-5">
-          <div className="font-bold text-lg">Others</div>
-          <div className="flex  gap-5 flex-wrap w-[50%]">
-            {others.map((item) => {
-              return (
-                <SkillsCard
-                  name={item.name}
-                  icon={item.icon}
-                  key={item.name}
-                ></SkillsCard>
-              );
-            })}
-          </div>
-        </div>{" "}
       </div>
+      <IconCloudSection></IconCloudSection>
     </div>
   );
 };
+
 export default Skills;
