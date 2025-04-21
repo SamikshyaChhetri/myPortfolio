236999bdf8d6e0a9e878e47d3b69db54a6d58cc4
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-04-21 22:29:01 +0545

    improved UI

--- src/components/SkillsCard.tsx
+++ src/components/SkillsCard.tsx
@@ -1,5 +1,4 @@
 import { FC, ReactNode } from "react";
-
 const SkillsCard: FC<{ icon: ReactNode; name: string }> = ({ icon, name }) => {
   return (
     <div className="border-2 border-black rounded-lg flex flex-col items-center p-2 h-20 w-20 bg-gray-100">
