d61c9f1129c786154672f3d379c2c6e38dab45ec
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-05-02 18:17:23 +0545

    feat: wrap page component with QueryClientProvider for React Query integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,23 @@ import Home from "@/components/Home";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 const page = () => {
   return (
     <div className="w-full scroll-smooth ">
-      <BackgroundBeamsWithCollision className="flex flex-col bg-gradient-to-br from-purple-50 via-white to-purple-100 ">
-        <Header></Header>
+      <QueryClientProvider client={queryClient}>
+        <BackgroundBeamsWithCollision className="flex flex-col bg-gradient-to-br from-purple-50 via-white to-purple-100 ">
+          <Header></Header>
 
-        <div className="h-20"></div>
+          <div className="h-20"></div>
 
-        <Home></Home>
-      </BackgroundBeamsWithCollision>
-      <Skills></Skills>
-      <Projects></Projects>
-      <Contact></Contact>
+          <Home></Home>
+        </BackgroundBeamsWithCollision>
+        <Skills></Skills>
+        <Projects></Projects>
+        <Contact></Contact>
+      </QueryClientProvider>
     </div>
   );
 };
